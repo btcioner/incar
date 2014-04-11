ab2bbea740a3266f9d3d95db02c0f24d1f8d766e
AceBear <vast_m_bear@yahoo.com>
2014-04-11 16:48:54 +0800

    ## 删除t_device_obd表

--- mysql/20140316A_Device.sql
+++ mysql/20140316A_Device.sql
@@ -1,19 +1,3 @@
--- 20140316 XGH OBD设备表
-CREATE TABLE IF NOT EXISTS t_device_obd(
-    obd_code VARCHAR(32) PRIMARY KEY COMMENT 'OBD设备唯一编码',
-    sim_number VARCHAR(16) COMMENT '插在OBD设备上的SIM卡的电话号码',
-    comment VARCHAR(32) COMMENT '简要说明性文字',
-    created_date DATE COMMENT '创建日期',
-    INDEX IX_DEVICE_OBD_1(sim_number)
-);
-
--- 20140329 此表仅供记录分配给组织的OBD设备,通常不应使用此表,通常应从车检索相关的OBD设备
-CREATE TABLE IF NOT EXISTS t_device_obd_org(
-    obd_code VARCHAR(32) COMMENT 'OBD设备唯一编码',
-    org_id INT UNSIGNED COMMENT '组织ID',
-    UNIQUE UNQ_DEVICE_OBD_ORG_1(obd_code, org_id),
-    INDEX IDX_DEVICE_OBD_ORG_2(org_id)
-);
 CREATE TABLE IF NOT EXISTS t_car_info(
     id int auto_increment   COMMENT '车辆编号',
     obd_code VARCHAR(32)    COMMENT 'OBD设备唯一编码',
@@ -25,12 +9,7 @@ CREATE TABLE IF NOT EXISTS t_car_info(
     mileage long            COMMENT '初始里程数',
     comment VARCHAR(32)     COMMENT '简要说明性文字',
     primary key (id));
-CREATE TABLE IF NOT EXISTS t_car_obd(
-    obd_code VARCHAR(32) COMMENT 'OBD设备唯一编码'),
-    car_id int COMMENT '车辆编号',
-    bind_time timestamp COMMENT '绑定时间',
-    unbind_time timestamp COMMENT '解除绑定时间'
-    );
+
 CREATE TABLE IF NOT EXISTS t_car_org(
     car_id int COMMENT '车辆编号',
     org_id int UNSIGNED COMMENT '组织ID',
