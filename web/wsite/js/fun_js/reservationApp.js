4138048c4cbfa550814a5eaebd901539efcd0954
jiangli <hyacinth0509@163.com>
2014-04-13 14:16:17 +0800

    none

---
+++ web/wsite/js/fun_js/reservationApp.js
@@ -0,0 +1,34 @@
+/**
+ * Created by liz on 14-03-29.
+ */
+
+// use ng-view for redirct load different page
+
+angular.module("SReservationApp", [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+]).config(function ($routeProvider, $locationProvider, $httpProvider) {
+    $routeProvider.
+        when('/main', {
+            controller: 's_reservationCtrl',
+            templateUrl: '/4sStore/partials/reservation_info.html'//主页显示统计信息
+        })
+        .when('/collapseGOne',{
+            controller:'s_reservationCtrl',
+            templateUrl:'/4sStore/partials/reservation_main.html'//显示全部保养信息
+        })
+        .when('/collapseGTwo',{
+            controller:'s_reservationCtrl',
+            templateUrl:'/4sStore/partials/repair_main.html'//显示全部维修信息
+         })
+        .when('/applyOper',{
+            controller:'s_reservationCtrl',
+            templateUrl:'/4sStore/partials/reservation_apply_oper.html'//新申请操作
+        })
+        .otherwise({
+           redirectTo:'/main'//跳转到预约服务的主界面
+        });
+       // $locationProvider.html5Mode(true);
+});
