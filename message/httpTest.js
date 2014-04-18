ad43a1b991df9aba9ecd4ff11e513e78e20cc3f5
Jesse Qu <jesse@baseware.net>
2014-04-18 21:57:53 +0800

    modified:   message/httpTest.js 	modified:   message/package.json

--- message/httpTest.js
+++ message/httpTest.js
@@ -1,15 +1,24 @@
+'use strict'
+
 var http = require("http");
+
 var data ={idArray:'0xFE05,0xFE06,0xFE07,0xFE08,0xFE09,0xFE0A,0xFE0B,0xFE0C'};
-data = require('querystring').stringify(data);
+
+data = JSON.stringify(data);
 console.log(data);
+
 var opt = {
-    method: "post",
+    method: "POST",
     host: "localhost",
     port: 1234,
-    path: "/message/send/18086620891/5665"
+    path: "/message/send/18086620891/5665",
+    headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(data)
+    }
 };
 
-var req = http.request(opt, function (serverFeedback) {
-});
+var req = http.request(opt, function (serverFeedback) {});
+
 req.write(data);
 req.end();
