2bcefc249743e8bab1d8e0d2628a4660b7ac1262
liuming <liuming@2014>
2014-05-22 18:05:30 +0800

    新数据库初始化脚本

--- server/core/initDrive319.js
+++ server/core/initDrive319.js
@@ -29,17 +29,6 @@ var sqlDetail="insert into t_drive_detail set ?";
 var detailArray=[];
 var driveArray=[];
 var driveCount=50;
-var faultCode=[
-    {
-        "code":"P0117",
-        "status":"存贮故障码",
-        "desc":"发动机冷却液温度传感器1电路低"
-    },{
-        "code":"P0195",
-        "status":"存贮故障码",
-        "desc":"机油温度传感器电路故障"
-    }
-];
 var carCondition=[
     {"id":0,"value":"8.83"},
     {"id":1,"value":"0"},
@@ -66,30 +55,61 @@ var carCondition=[
     {"id":28,"value":"0.170"},
     {"id":74,"value":"EOBD"}];
 var obdCode="WFQ00012925";
+var vid="11111";
+var vin="W0L0ZCF693108391A";
+//158913798138,158913805191
+var speedGroupArray=[0,80,160];
+
 for(var i=0;i<driveCount;i++){
-    var runtime=getRandom(25,200,0);
-    var fTime=getRandomTime(2014,4,13);
+    var runtime=getRandom(600,3600,0);
+    var fireTime=getRandomTime(2014,4,22);
+    var flameOutTime=new Date(fireTime.valueOf()+runtime*1000);
+    var lastUpdateTime=new Date(flameOutTime.valueOf()+60 * 1000);
+    var speedGroup=[];
+    for(var j=0;j<speedGroupArray.length;j++){
+        var speedUnit=speedGroupArray[j];
+        speedGroup.push({
+            speed:speedUnit,
+            time:getRandom(0,600,0),
+            distance:speedUnit===0?0:getRandom(1000,10000,0)
+        });
+    }
     var drive={
-        obdCode:obdCode,
-        vin:"W0L0ZCF693108391A",
-        brand:9,
-        series:84,
-        modelYear:0xFF,
-        firingVoltage:getRandom(7,10,1),
-        runTime:runtime,
-        currentMileage:getRandom(6,100,1),
-        currentAvgOilUsed:getRandom(7,12,2),
-        speedingTime:getRandom(0,30,0),
-        speedUp:getRandom(0,10,0),
-        speedDown:getRandom(0,10,0),
-        sharpTurn:getRandom(0,10,0),
-        flameVoltage:getRandom(7,10,1),
-        avgOilUsed:getRandom(7,12,2),
-        mileage:getRandom(0,10000,0),
-        voltageAfter:getRandom(7,10,1),
-        carStatus:4,
-        fireTime:fTime,
-        flameOutTime:new Date(fTime.valueOf()+runtime* 60 * 1000)
+        obdCode:obdCode,                        //OBD设备号
+        tripId:i,                               //行程标识
+        vid:vid,                                //车辆标识
+        vin:vin,                                //发动机标识
+        lastUpdateTime:lastUpdateTime,          //最后更新时间
+        fireTime:fireTime,                      //点火时间
+        firingVoltage:getRandom(7,10,1),        //点火电压
+        fireSpeed:getRandom(10,50,0),           //点火定位车速
+        fireDistance:getRandom(100,1000,0),     //点火定位行驶距离
+        fireLongitude:'W'+getRandom(0,180,6),   //点火定位经度
+        fireLatitude:'S'+getRandom(0,90,6),     //点火定位纬度
+        fireDirection:getRandom(0,360,0),       //点火定位方向
+        fireLocationTime:fireTime,              //点火定位时间
+        fireLocationType:1,                     //点火定位方式(1-基站定位,2-GPS定位)
+        runTime:runtime,                        //本次行程持续时间
+        currentMileage:getRandom(6,100,1),      //本次驾驶行驶里程
+        currentAvgOilUsed:getRandom(7,12,2),    //本次驾驶平均油耗
+        mileage:getRandom(0,10000,0),           //累计行驶里程
+        avgOilUsed:getRandom(7,12,2),           //累计平均油耗
+        speedGroup:JSON.stringify(speedGroup),  //本行程车速分组统计(JSON)
+        speedingTime:getRandom(0,30,0),         //超速行驶时间
+        speedUp:getRandom(0,10,0),              //急加速次数
+        speedDown:getRandom(0,10,0),            //急减速次数
+        sharpTurn:getRandom(0,10,0),            //急转弯次数
+        speedMax:getRandom(60,250,0),           //最高车速
+        flameOutSpeed:getRandom(10,50,0),       //熄火定位车速
+        flameOutDistance:getRandom(100,1000,0), //熄火定位行驶距离
+        flameOutLongitude:'W'+getRandom(0,180,6),//熄火定位经度
+        flameOutLatitude:'S'+getRandom(0,90,6), //熄火定位纬度
+        flameOutDirection:getRandom(0,360,0),   //熄火定位方向
+        flameOutLocationTime:fireTime,          //熄火定位时间
+        flameOutLocationType:1,                 //熄火定位方式(1-基站定位,2-GPS定位)
+        flameOutVoltage:getRandom(7,10,1),      //熄火时蓄电池电压
+        carStatus:4,                            //车辆当前状态(启动、行驶、熄火、完成、异常)12345
+        flameOutTime:flameOutTime               //熄火时间
     };
     driveArray.push(drive);
 }
@@ -100,15 +120,11 @@ for(i=0;i<driveArray.length;i++){
         console.log("成功添加行车数据"+driveId+"--"+JSON.stringify(drive));
         var fireTime=drive.fireTime;
         var flameOutTime=drive.flameOutTime;
-        var fCode=getRandom(0,10)>2?"[]":JSON.stringify(faultCode);
         while(fireTime<flameOutTime){
             var detail={
                 obdCode:obdCode,
                 obdDriveId:driveId,
-                faultCode:fCode,
-                carCondition:JSON.stringify(carCondition),
-                avgOilUsed:getRandom(8,12,1),
-                mileage:getRandom(0,10,2),
+                detail:JSON.stringify(carCondition),
                 createTime:fireTime
             }
             detailArray.push(detail);
