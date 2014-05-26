2655a12433b4e3bfa9e14826103e0506fb525ee7
zhoupeng <zhou750516@163.com>
2014-05-26 17:02:46 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits) #5.26.“设置”

--- web/mservice/brandService.js
+++ web/mservice/brandService.js
@@ -26,8 +26,8 @@ function brandData(req, res) {
 function getBrand(db, callback) {
     var pool = db();
     var report = new Array();
-    console.log('brand search begin');
-    pool.query('select brandCode from t_car_dictionary ;',[], function(err, rows){
+   // console.log('brand search begin');
+    pool.query('select brandCode from t_car_dictionary group by brandCode;',[], function(err, rows){
         if (err) { callback(err); }
         else {
             console.log('brand length'+rows.length);
