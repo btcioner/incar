1d7eeb26bad28ad8fbf95799d21adad1752f788d
zhoupeng <zhou750516@163.com>
2014-05-27 14:21:26 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits) #5.26.“设置”

--- web/mservice/brandService.js
+++ web/mservice/brandService.js
@@ -27,21 +27,20 @@ function getBrand(db, callback) {
     var pool = db();
     var report = new Array();
    // console.log('brand search begin');
-    pool.query('select brandCode from t_car_dictionary group by brandCode;',[], function(err, rows){
+    pool.query('select brandCode,brand from t_car_dictionary group by brandCode;',[], function(err, rows){
         if (err) { callback(err); }
         else {
             if(rows){
                for(var i=0;i<rows.length;i++){
                    var brandData={};
                    brandData.id=rows[i].brandCode;
-                   var bcode=rows[i].brandCode;
-                   //pool.query("SET character_set_client=utf8,character_set_connection=utf8");
+                   brandData.brand =rows[i].brand;
+                  //pool.query("SET character_set_client=utf8,character_set_connection=utf8");
                    pool.query('select seriesCode,series from t_car_dictionary where brandCode=?;',[rows[i].brandCode],function(err, srows){
                        if(err){ callback(err);}
                        else {
                            if(srows){
-                               console.log('brandCode:'+bcode+'   brand length:'+srows.length+'  brand:'+srows[0].brand);
-                               brandData.brand =srows[0].brand;
+                               console.log('brandCode:'+bcode+'   brand length:'+srows.length+'  brand:'+brandData.brand);
                                var items=new Array();
                                for(var j=0;j<srows.length;j++){
                                    var series={};
