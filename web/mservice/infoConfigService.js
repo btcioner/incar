f6ba1383b57d84218791e7cde8211ad9b74b36bb
zhoupeng <zhou750516@163.com>
2014-05-27 11:04:39 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits) #5.26.“设置”

--- web/mservice/infoConfigService.js
+++ web/mservice/infoConfigService.js
@@ -70,9 +70,13 @@ function getCarInfo(db, callback){
                             report.license=rows[0].license;
                             report.obd_code=rows[0].obd_code;
                             report.brand=rows[0].brand;
-                            getBrandName(db,rows[0].brand);
+                            getBrandName(db,rows[0].brand,function(err,data){
+                                report.brandName=data;
+                            });
                             report.series=rows[0].series;
-                            getSeriesName(db,rows[0].series);
+                            getSeriesName(db,rows[0].series,function(err,data){
+                                report.seriesName=data;
+                            });
                             report.modelYear=rows[0].modelYear;
                             report.disp=rows[0].disp;
                             report.mileage=rows[0].mileage;
@@ -86,12 +90,12 @@ function getCarInfo(db, callback){
         }
     });
 }
-function getBrandName(db,brandCode){
+function getBrandName(db,brandCode,callback){
     var pool = db();
     pool.query('select brand from t_car_dictionary where brandCode=?;',[brandCode],function(err,rows){
-        if(err) {}
+        if(err) {callback(err)}
         else{
-          report.brandName=rows[0].brand;
+            callback(null,rows[0].brand);
              }
     });
 }
@@ -100,7 +104,7 @@ function getSeriesName(db,seriesCode,callback){
     pool.query('select series from t_car_dictionary where seriesCode=?;',[seriesCode],function(err,rows){
         if(err) callback(err);
         else{
-                report.seriesName=rows[0].series;
+            callback(null,rows[0].series);
          }
     });
 }
