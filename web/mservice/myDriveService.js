d9821e1acccc69c5152e04a30f37d4d85c605ba6
zhoupeng <zhou750516@163.com>
2014-05-28 16:00:49 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits) #5.28.“预约”

--- web/mservice/myDriveService.js
+++ web/mservice/myDriveService.js
@@ -513,7 +513,8 @@ function getSpeedForLastTime(db, obdCode, callback){
         if (err) { callback(err); }
         else {
             if (rows && rows.length === 1) {
-                var speedJsonArray=rows[0].speedGroup;
+                var speedJsonArray=new Array();
+                speedJsonArray=rows[0].speedGroup;
                 console.log(speedJsonArray);
                 var speedJson={};
                 speedJson={'slow':speedJsonArray[0].distance,'middle':speedJsonArray[1].distance,'high':speedJsonArray[2].distance}
