014cb6895b390a63db4f8facb1d9509fb03ad76b
AceBear <vast_m_bear@yahoo.com>
2014-05-28 10:47:05 +0800

    t_activity_member

--- mysql/20140527A_Activity.sql
+++ mysql/20140527A_Activity.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS t_activity_template(
     template VARCHAR(32) NOT NULL COMMENT '模版内部名称',
     title VARCHAR(64) NOT NULL COMMENT '活动标题',
     brief VARCHAR(4096) COMMENT '活动简述',
-    awards VARCHAR(4096) COMMENT '活动颁奖简述'
+    awards VARCHAR(4096) COMMENT '活动颁奖简述',
+    INDEX IDX_ACT_1(s4_id,name)
 );
 
 -- 活动通用信息
@@ -18,8 +19,22 @@ CREATE TABLE IF NOT EXISTS t_activity(
     brief VARCHAR(4096) COMMENT '简述',
     awards VARCHAR(4096) COMMENT '颁奖简述',
     status TINYINT NOT NULL DEFAULT '1' COMMENT '状态1待公告 2已公告 3正在进行 4结束 5结果已发布 6取消',
+    logo_url VARCHAR(256) COMMENT 'LOGO图片URL',
     tm_announce DATETIME COMMENT '公告时间',
     tm_start DATETIME COMMENT '开始时间',
     tm_end DATETIME COMMENT '结束时间',
-    tm_publish DATETIME COMMENT '结果发布时间'
+    tm_publish DATETIME COMMENT '结果发布时间',
+    INDEX IDX_ACT_1(s4_id,template_id)
+);
+
+-- 活动参与人
+CREATE TABLE IF NOT EXISTS t_activity_member(
+    act_id INT UNSIGNED NOT NULL COMMENT '活动ID',
+    cust_id INT UNSIGNED NOT NULL COMMENT '活动参加者的ID',
+    status TINYINT NOT NULL DEFAULT '0' COMMENT '0邀请 1报名 2参加 3客户主动退出 4条件不符被拒',
+    ref_car_id INT UNSIGNED COMMENT '参考车的ID,因为TAG打在车上',
+    ref_tags VARCHAR(256) COMMENT '逗号分割的参考TAG_ID',
+    ref_tag_tm DATETIME COMMENT '参考TAG标记时间',
+    UNIQUE UNQ_ACT_MEMBER(act_id,cust_id),
+    INDEX IDX_ACT_MEMBER_1(act_id,status)
 );
