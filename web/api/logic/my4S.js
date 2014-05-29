c8051e0487ace2ab6a110dde02b764fcca073b0d
zhoupeng <zhou750516@163.com>
2014-05-29 13:38:04 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits) #5.28.“预约”

--- web/api/logic/my4S.js
+++ web/api/logic/my4S.js
@@ -99,18 +99,14 @@ my4S.book = function(userName, session, callback){
 };
 my4S.trialrun=function(userName, session, callback){
     var tpl = [
-        '本店提供：\n\n',
-        '<% var idx = 1; %>',
-        '<% slots.forEach(function(slot){ %>',
-        '<%=idx%>:  <%= slot.time.Format("yyyy-MM-dd hh:mm:ss") %>\n\n',
-        '<% idx++; %>',
-        '<% }); %>',
-        '\n',
-        '回复特价工位号进行预约\n\n',
-        '或点击本消息进行其他时段预约\n\n',
+        '本店提供XXX品牌试乘试驾：\n\n',
+
+        '请点击进入详情进行预约\n\n',
+        '',
         '\n'
     ].join('');
     var compiled = ejs.compile(tpl);
+    callback(null,compiled);
 }
 my4S.manual = function(userName, session, callback){
     // 模板将来要从数据库来读取
