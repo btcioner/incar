511a47a50d8ca20d678b2030c899f2a663cad111
zhoupeng <zhou750516@163.com>
2014-05-29 10:40:02 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits) #5.28.“预约”

--- mysql/20140506C_Promotion.sql
+++ mysql/20140506C_Promotion.sql
@@ -35,3 +35,19 @@ CREATE TABLE IF NOT EXISTS t_slot_booking(
 
 	INDEX IX_PROMOTION_SLOT_1(id)
 );
+create table IF NOT EXISTS `t_trialrun`(
+   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自动增长id',
+   `wx_oid` varchar(512) COMMENT 'openid:sopenid格式，未注册用户以此标识',
+   `acc_id` int(11) COMMENT '已注册用户，账号id',
+   `bookingtime` datetime COMMENT '预约时间',
+   `seriesName` varchar(30) COMMENT '车型',
+   `name` varchar(10) COMMENT '姓名，已注册用户可从t_account读出',
+   `phone` varchar(20) COMMENT '电话，已注册用户可从t_account读出',
+    channel VARCHAR(32) NOT NULL COMMENT '申请渠道 weixin mobileApp website telephone',
+    channel_specific VARCHAR(256) COMMENT '申请渠道指定信息',
+   `bookStatus` int(4) DEFAULT '0' COMMENT '预约状态：0-无效 1-已申请待确认 2-被拒 3-成功批准 4-被取消 ',
+
+   tc VARCHAR(32) COMMENT '记录操作用户名',
+   ts timestamp COMMENT '记录操作时间戳',
+   PRIMARY KEY (`id`)
+ )
