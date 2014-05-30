18fde563f3193f32fa68fd971c497e7f887c653c
zhoupeng <zhou750516@163.com>
2014-05-30 15:32:01 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits)

--- web/mservice/brandService.js
+++ web/mservice/brandService.js
@@ -14,23 +14,38 @@ function brandData(req, res) {
     var postData = req.body;
     console.log(postData);
     var db = this.db;
-    var sopenid=postData.user.split('@')[1];
-    console.log(sopenid+"00000000000");
-    getBrand(db, sopenid ,function(err, data) {
-         if (err) { res.send(err); }
-          else {
-           //console.log('Car brands print:\n');
-           console.log(data);
-           res.send(data);
-         }
-    });
+    var sopenid= postData.user.split('@')[1];
+    if(null==sopenid){
+        getBrand(db, postData.user ,function(err, data) {
+            if (err) { res.send(err); }
+            else {
+                //console.log('Car brands print:\n');
+                console.log(data);
+                res.send(data);
+            }
+        });
+    }else{
+        getBrandForNew(db, sopenid ,function(err, data) {
+            if (err) { res.send(err); }
+            else {
+                //console.log('Car brands print:\n');
+                console.log(data);
+                res.send(data);
+            }
+        });
+    }
+
  }
 
-function getBrand(db, sopenid,callback) {
+function getBrand(db, username,callback) {
     var pool = db();
     var report ={};
    // console.log('brand search begin');
-   pool.query('select brand from t_4s where openid=?;',[sopenid],function(err,rows){
+    pool.query('select s4_id from t_account where wx_oid like ?;',["%"+username+"%"],function(err,rows){
+        if(err){callback(err);}
+        else{
+            if(rows&&rows.length===1){
+                pool.query('select brand from t_4s where id=?;',[rows[0].s4_id],function(err,rows){
                     if(err){callback(err);}
                     else{
                         if(rows&&rows.length===1){
@@ -50,7 +65,6 @@ function getBrand(db, sopenid,callback) {
                                                     seriesData[i].seriesCode=rows[i].seriesCode;
                                                 }
                                                 report.seriesData=seriesData;
-                                                console.log(report);
                                                 callback(null,report);
                                             }else{callback(new Error('no series data.'));}
                                         }
@@ -61,5 +75,41 @@ function getBrand(db, sopenid,callback) {
                         else {callback(new Error('4s has no brand.'));}
                     }
                 });
-
+            }else {callback(new Error('user has no 4s.'));}
+        }
+    });
+}
+function getBrandForNew(db, spoenid,callback){
+    var pool = db();
+    var report ={};
+    pool.query('select brand from t_4s where openid=?;',[spoenid],function(err,rows){
+        if(err){callback(err);}
+        else{
+            if(rows&&rows.length===1){
+                pool.query('select brandCode,brand from t_car_dictionary where brandCode=?;',[rows[0].brand], function(err, rows){
+                    if(err){callback(err);}
+                    else{
+                        report.brand=rows[0].brand;
+                        report.brandCode=rows[0].brandCode;
+                        pool.query('select seriesCode,series from t_car_dictionary where brandCode=?;',[rows[0].brandCode],function(err,rows){
+                            if(err){callback(err);}
+                            else{
+                                if(rows){
+                                    var seriesData=new Array();
+                                    for(var i=0;i<rows.length;i++){
+                                        seriesData[i]={};
+                                        seriesData[i].series=rows[i].series;
+                                        seriesData[i].seriesCode=rows[i].seriesCode;
+                                    }
+                                    report.seriesData=seriesData;
+                                    callback(null,report);
+                                }else{callback(new Error('no series data.'));}
+                            }
+                        });
+                    }
+                });
+            }
+            else {callback(new Error('4s has no brand.'));}
+        }
+    });
 }
