945f58894514c1aef824ab14c1cf12e8095cdc54
zhoupeng <zhou750516@163.com>
2014-05-30 13:34:16 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits)

--- web/mservice/trialrunService.js
+++ web/mservice/trialrunService.js
@@ -25,9 +25,11 @@ function trialrun(req, res) {
 
 function submitTrialrun(db, postData,callback) {
     var pool = db();
-
+    var temp=postData.bookTime.split(":");
+    postData.bookDate=new Date(postData.bookDate);
+    postData.bookDate.setHours(temp[0],temp[1]);
     pool.query('insert into  t_trialrun (wx_oid,acc_id,bookingtime,seriesName,name,phone,tc,ts) values(?,?,?,?,?,?,?,now());',
-        [postData.wx_oid,postData.acc_id,postData.bookingtime,postData.seriesName,postData.name,postData.phone,''],function(err,rows){
+        [postData.wx_oid,postData.acc_id,postData.bookDate,postData.seriesName,postData.name,postData.phone,''],function(err,rows){
         if(err){callback(err);}
         else{
            callback(null,1);
