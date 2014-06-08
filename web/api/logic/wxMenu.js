551848ba337f981938f1683047519b81095eda66
zhoupeng <zhou750516@163.com>
2014-06-08 16:20:37 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits)

--- web/api/logic/wxMenu.js
+++ web/api/logic/wxMenu.js
@@ -75,12 +75,12 @@ wxMenu.defineWXMenu = function(appid, appsecret){
                                 {
                                     "type":"view",
                                     "name":"我的活动",
-                                    "url":"http://linuxsrv.winphone.us/shopping"
+                                    "url":"http://linuxsrv.winphone.us/msite/getCodeForActivity.html"
                                 },
                                 {
                                     "type":"view",
                                     "name":"我的预约",
-                                    "url":"http://linuxsrv.winphone.us/promotions"
+                                    "url":"http://linuxsrv.winphone.us/msite/getCodeForBook.html"
                                 },
                                 {
                                     "type":"view",
