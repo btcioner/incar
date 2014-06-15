18b335e77971fe7f537376f54407cba1b50c5b6b
zhoupeng <zhou750516@163.com>
2014-06-15 17:15:18 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits)

--- web/mservice/myActivityService.js
+++ web/mservice/myActivityService.js
@@ -62,27 +62,3 @@ function search(db,acc_id,s4id,callback) {
             }
         });
 }
-function ActivityInfo(db,act_id,s4id,status,callback){
-    var pool = db();
-    pool.query('select id,title,status,tm_announce  from  t_activity where id = ? and s4_id=? order by tm_announce desc;',
-        [act_id,s4id],function(err,rows){
-            if(err){callback(err);}
-            else{console.log("rows:"+rows[0].title);
-                if(rows&&rows.length==1){
-                    console.log("aa"+rows[0].id);
-                    console.log("aa"+rows[0].title);
-                    console.log("aa"+rows[0].status);
-                    console.log("aa"+rows[0].tm_announce);
-                    console.log("aa"+rows[0].status);
-                    var act_data={};
-                    act_data.id=rows[0].id;
-                    act_data.title=rows[0].title;
-                    act_data.status=rows[0].status;
-                    act_data.tm_announce=rows[0].tm_announce;
-                    act_data.myStatus=status;
-                    console.log("actData:"+act_data);
-                   callback(null,act_data);
-                }else callback(new Error("t_activity data error."));
-            }
-        });
-}
