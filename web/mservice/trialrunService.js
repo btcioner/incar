b8454467e0eaa8ee76fc65f99d4b9d69c6343dea
zhoupeng <zhou750516@163.com>
2014-06-15 11:20:20 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits)

--- web/mservice/trialrunService.js
+++ web/mservice/trialrunService.js
@@ -32,7 +32,16 @@ function submitTrialrun(db, postData,callback) {
         [postData.wx_oid,postData.acc_id,postData.bookDate,postData.seriesName,postData.name,postData.phone,''],function(err,rows){
         if(err){callback(err);}
         else{
-           callback(null,1);
+            pool.query('select id from t_trialrun where wx_oid like ? order by ts desc limit 1',["%"+postData.wx_oid+'%'],function(err,rows){
+                if(err){ callback(err);}
+                else if(rows){
+                    pool.query('insert into t_work (work,step,work_ref_id,org_id,cust_id,working_time,created_time) value(?,?,?,?,?,?,now())',
+                        ['drivetry','applied',rows[0].id,postData.s4_id,postData.acc_id,postData.bookDate],function(err,result){
+                            if(err){  callback(err);}
+                            else callback(null,1);
+                        });
+                }else callback(new Error("trialrun data insert Error."));
+            });
          }
     });
 
