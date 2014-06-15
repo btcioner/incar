76751176be80de3ce5af38a711808dd4cda78bac
zhoupeng <zhou750516@163.com>
2014-06-15 17:55:01 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Your branch is ahead of 'origin/master' by 7 commits. #   (use "git push" to publish your local commits)

--- web/mservice/myActivityService.js
+++ web/mservice/myActivityService.js
@@ -58,7 +58,7 @@ function search(db,acc_id,s4id,callback) {
                                        act_data.status=result[0].status;
                                        act_data.tm_announce=result[0].tm_announce;
                                        act_data.myStatus=st;
-                                       myActData[i]=act_data;
+                                       myActData.push(act_data);
                                        act_detail.n++;
                                        act_detail.end();
                                     }else callback(new Error("t_activity data error."));
