667f35d247185dbe10b3201c4f54cfc5e7fbe5ab
AceBear <vast_m_bear@yahoo.com>
2014-06-25 18:48:40 +0800

    fix

--- mysql/install.sh
+++ mysql/install.sh
@@ -1,9 +1,10 @@
- mysql -u root -pInCar2014 -D incar < 20140312A_SYS.sql
- mysql -u root -pInCar2014 -D incar < 20140313A_OBD.sql
- mysql -u root -pInCar2014 -D incar < 20140506A_Business.sql
- mysql -u root -pInCar2014 -D incar < 20140506B_Staff.sql
- mysql -u root -pInCar2014 -D incar < 20140506C_Promotion.sql
- mysql -u root -pInCar2014 -D incar < 20140506D_Content.sql
- mysql -u root -pInCar2014 -D incar < 20140506E_Work.sql
- mysql -u root -pInCar2014 -D incar < 20140527A_Activity.sql
- mysql -u root -pInCar2014 -D incar < 20140528A_ActSaveGas.sql
+#!/bin/sh
+mysql -u root -pInCar2014 -D incar < 20140312A_SYS.sql
+mysql -u root -pInCar2014 -D incar < 20140313A_OBD.sql
+mysql -u root -pInCar2014 -D incar < 20140506A_Business.sql
+mysql -u root -pInCar2014 -D incar < 20140506B_Staff.sql
+mysql -u root -pInCar2014 -D incar < 20140506C_Promotion.sql
+mysql -u root -pInCar2014 -D incar < 20140506D_Content.sql
+mysql -u root -pInCar2014 -D incar < 20140506E_Work.sql
+mysql -u root -pInCar2014 -D incar < 20140527A_Activity.sql
+mysql -u root -pInCar2014 -D incar < 20140528A_ActSaveGas.sql
