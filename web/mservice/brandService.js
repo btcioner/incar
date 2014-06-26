eda027b76fd9aedb77bfca9f9e22d04b2650267c
AceBear <vast_m_bear@yahoo.com>
2014-06-26 17:17:21 +0800

    fix enroll

--- web/mservice/brandService.js
+++ web/mservice/brandService.js
@@ -17,7 +17,17 @@ function brandData(req, res) {
     var sopenid= postData.user.split('@')[1];
     if(null==sopenid){
         getBrand(db, postData.user ,function(err, data) {
-            if (err) { res.send(err); }
+            if (err) {
+                // try to use the appid to resolve the brand
+                if(postData.appid){
+                    getBrandFromAppId(postData.appid, function(ex, data){
+                        if(ex) console.log(ex);
+                        else res.send(data);
+                    });
+                }
+                else
+                    res.send(err);
+            }
             else {
                 //console.log('Car brands print:\n');
                 console.log(data);
@@ -86,6 +96,41 @@ function getBrandForNew(db, spoenid,callback){
     var pool = db();
     var report ={};
     pool.query('select brand from t_4s where openid=?;',[spoenid],function(err,rows){
+        if(err){callback(err);}
+        else{
+            if(rows&&rows.length===1){
+                pool.query('select brandCode,brand from t_car_dictionary where brandCode=?;',[rows[0].brand], function(err, rows){
+                    if(err){callback(err);}
+                    else{
+                        report.brand=rows[0].brand;
+                        report.brandCode=rows[0].brandCode;
+                        pool.query('select seriesCode,series from t_car_dictionary where brandCode=?;',[rows[0].brandCode],function(err,rows){
+                            if(err){callback(err);}
+                            else{
+                                if(rows){
+                                    var seriesData=new Array();
+                                    for(var i=0;i<rows.length;i++){
+                                        seriesData[i]={};
+                                        seriesData[i].series=rows[i].series;
+                                        seriesData[i].seriesCode=rows[i].seriesCode;
+                                    }
+                                    report.seriesData=seriesData;
+                                    callback(null,report);
+                                }else{callback(new Error('no series data.'));}
+                            }
+                        });
+                    }
+                });
+            }
+            else {callback(new Error('4s has no brand.'));}
+        }
+    });
+}
+
+function getBrandFromAppId(appid, cb){
+    var pool = db();
+    var report ={};
+    pool.query('select brand from t_4s where wx_app_id=?;',[appid],function(err,rows){
         if(err){callback(err);}
         else{
             if(rows&&rows.length===1){
