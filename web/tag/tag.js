c195437ce903e026fcbbe33b0e15ef0d83176b96
liuming <liuming@2014>
2014-07-01 14:16:25 +0800

    dao相关修改

--- web/tag/tag.js
+++ web/tag/tag.js
@@ -518,40 +518,46 @@ exports.searchByTags= function(req,res){
         var sql="select g.id as groupId,g.type as groupType,t.id as tagId " +
             "from t_tag_group g " +
             "left join t_tag t on t.groupId=g.id"
-        dao.findBySql(sql,[],function(rows){
-            var tagMap={};
-            for(var i=0;i<rows.length;i++){
-                var groupId=rows[i].groupId;
-                var tagId=rows[i].tagId;
-                var type=rows[i].groupType;
-                tagMap[tagId]={groupId:groupId,type:type};
-            }
-            var tagList={};
-            for(i=0;i<tagArray.length;i++){
-                var tagId=tagArray[i];
-                var tagInfo=tagMap[tagId];
-                if(!tagInfo)continue;
-                var groupId=tagInfo.groupId;
-                var type=tagInfo.type;
-
-                var t=tagList[groupId];
-
-                if(!t){
-                    t={type:type,tags:[]};
-                    tagList[groupId]=t;
-                }
-                t.tags.push(tagId);
-            }
-            var sqlBuild=buildSearchSql(tagList);
-            sql=sqlBuild.sql;
-            var args=sqlBuild.args;
-            if(args.length>0){
-                dao.findBySql(sql,args,function(rows){
-                    res.json(rows);
-                });
+        dao.findBySql(sql,[],function(info){
+            if(info.err){
+                res.json(info);
             }
             else{
-                res.json({status:'failure'});
+                var rows=info.data;
+                var tagMap={};
+                for(var i=0;i<rows.length;i++){
+                    var groupId=rows[i].groupId;
+                    var tagId=rows[i].tagId;
+                    var type=rows[i].groupType;
+                    tagMap[tagId]={groupId:groupId,type:type};
+                }
+                var tagList={};
+                for(i=0;i<tagArray.length;i++){
+                    var tagId=tagArray[i];
+                    var tagInfo=tagMap[tagId];
+                    if(!tagInfo)continue;
+                    var groupId=tagInfo.groupId;
+                    var type=tagInfo.type;
+
+                    var t=tagList[groupId];
+
+                    if(!t){
+                        t={type:type,tags:[]};
+                        tagList[groupId]=t;
+                    }
+                    t.tags.push(tagId);
+                }
+                var sqlBuild=buildSearchSql(tagList);
+                sql=sqlBuild.sql;
+                var args=sqlBuild.args;
+                if(args.length>0){
+                    dao.findBySql(sql,args,function(info){
+                        res.json(info);
+                    });
+                }
+                else{
+                    res.json({status:'failure'});
+                }
             }
         });
     }
@@ -596,105 +602,3 @@ function buildSearchSql(tagList){
     var sql=sqlStart+sqlJoin+sqlWhere+sqlCustomerTag;
     return {sql:sql,args:args};
 }
-
-//buildSearchSql({});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var tagGroup=[
-    {name:"车系",description:"车系",type:0},
-    {name:"渠道",description:"车主的来源",type:0},
-    {name:"车龄",description:"车的驾驶年限",type:0},
-    {name:"用途",description:"标识车是商用还是家用",type:0},
-    {name:"用车时段",description:"用来描述车主的用车时段",type:0},
-    {name:"用车频率",description:"用来描述车主的用车频率",type:0},
-    {name:"驾驶偏好",description:"用来描述车主的驾驶偏好",type:0},
-    {name:"自定义标签",description:"暗箱操作专用",type:1}
-];
-function initTagGroup(){
-    var sql="insert into t_tag_group set ?";
-    for(var i=0;i<tagGroup.length;i++){
-        dao.insertBySql(sql,tagGroup[i],function(info,args){
-            args.id=info.insertId;
-            console.log(args);
-        });
-    }
-}
-function initCarBrandForTag(){
-    var sql="select * from t_car_dictionary";
-    dao.findBySql(sql,[],function(rows){
-        for(var i=0;i<rows.length;i++){
-            var cd=rows[i];
-            var code='ser'+cd.brandCode+'-'+cd.seriesCode;
-            var name=cd.series;
-            var description=cd.description;
-            var tag={
-                code:code,
-                name:name,
-                description:name,
-                active:1,
-                groupId:1,
-                createTime:new Date(),
-                creator:'柳明'
-            };
-            sql="insert into t_tag set ?";
-            dao.insertBySql(sql,tag,function(info,args){
-                args.id=info.insertId;
-                console.log("成功添加车系标签："+JSON.stringify(args));
-            });
-        }
-    });
-}
-function initOtherTag(){
-    var tags=[
-        {code:'chl1',name:"APP",description:"手机客户端",active:1,groupId:6,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'chl2',name:"微信",description:"微信端",active:1,groupId:6,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'chl3',name:"电话",description:"电话营销",active:1,groupId:6,createTime:new Date(),creator:'柳明',s4Id:0},
-
-        {code:'age0',name:"不到一年",description:"不到一年",active:1,groupId:5,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'age1',name:"一到两年",description:"一到两年",active:1,groupId:5,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'age2',name:"两到三年",description:"两到三年",active:1,groupId:5,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'age3',name:"三到四年",description:"三到四年",active:1,groupId:5,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'age4',name:"四到五年",description:"四到五年",active:1,groupId:5,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'age5',name:"五年以上",description:"五年以上",active:1,groupId:5,createTime:new Date(),creator:'柳明',s4Id:0},
-
-        {code:'useTo1',name:"商用",description:"商业用车",active:1,groupId:2,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'useTo2',name:"家用",description:"家庭用车",active:1,groupId:2,createTime:new Date(),creator:'柳明',s4Id:0},
-
-        {code:'time1',name:"上下班",description:"6:00-10:00,17:00-20:00使用",active:1,groupId:7,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'time2',name:"工作时",description:"10:00-17:00使用",active:1,groupId:7,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'time3',name:"非工作时段",description:"20:00-6:00使用",active:1,groupId:7,createTime:new Date(),creator:'柳明',s4Id:0},
-
-        {code:'rate1',name:"极低",description:"很少很少",active:1,groupId:3,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'rate2',name:"低",description:"很少",active:1,groupId:3,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'rate3',name:"一般",description:"一般",active:1,groupId:3,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'rate4',name:"高",description:"很多",active:1,groupId:3,createTime:new Date(),creator:'柳明',s4Id:0},
-
-        {code:'pre1',name:"保守",description:"保守",active:1,groupId:4,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'pre2',name:"普通",description:"普通",active:1,groupId:4,createTime:new Date(),creator:'柳明',s4Id:0},
-        {code:'pre3',name:"豪放",description:"豪放",active:1,groupId:4,createTime:new Date(),creator:'柳明',s4Id:0}
-    ];
-    var sql="insert into t_tag set ?";
-    for(var i=0;i<tags.length;i++){
-        dao.insertBySql(sql,tags[i],function(info,args){
-            args.id=info.insertId;
-            console.log(args);
-        });
-    }
-}
-//initTagGroup();
-//initCarBrandForTag();
-//initOtherTag();
