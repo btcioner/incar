e1f5b0b88698eb13279365593b57c7e54dea5ee6
AceBear <vast_m_bear@yahoo.com>
2014-07-05 01:21:59 +0800

    fix wechat menu define process

--- web/api/logic/menu.js
+++ web/api/logic/menu.js
@@ -8,11 +8,33 @@ var mysql = require('mysql');
 var config = require('../../config/config');
 var db = require('../../config/db');
 var WXAPI = require('../weixin').API;
+var url = require('url');
 
 exports = module.exports = function(tickTasks, menuObject, callback) {
+
+    // resolve url based on the wx_oauth_addr
+    var resolveUrl = function(mo, baseSite){
+        // clone mo
+        var mx = JSON.parse(JSON.stringify(mo));
+        // process url
+        if(baseSite){
+            var baseUrl = "http://" + baseSite;
+            for(var i in mx.button){
+                var btn = mx.button[i];
+                for(var j in btn.sub_button){
+                    var sub = btn.sub_button[j];
+                    if(sub.url){
+                        sub.url = url.resolve(baseUrl, sub.url);
+                    }
+                }
+            }
+        }
+        return mx;
+    };
+
     var pool = db();
 
-    var sql = 'select id,name,wx_app_name appName, wx_app_id appId, wx_app_secret appSecret from t_4s ;';
+    var sql = 'select id,name,wx_app_name appName, wx_app_id appId, wx_app_secret appSecret, wx_oauth_addr from t_4s ;';
 
     pool.query(sql, function(err, rows){
         if (err) { return callback(err, null); }
@@ -20,12 +42,13 @@ exports = module.exports = function(tickTasks, menuObject, callback) {
             rows.forEach(function(element, index, array){
                 tickTasks.enqueueTask(function() {
                     var api = new WXAPI(element.appId, element.appSecret);
-                    api.createMenu(menuObject, function(err, result){
+
+                    api.createMenu(resolveUrl(menuObject, element.wx_oauth_addr), function(err, result){
                         if (err) {
                             console.log('4s store: '+element.name+' weixin menu initial error.   4s store id :'+element.id+'\n' + err + '\n');
                         }
                         else {
-                            console.log('Weixin menu was newly defined!!\n');
+                            console.log('Weixin menu was newly defined for "' + element.name + '" to ' + element.wx_oauth_addr +  ' !!!\n');
                         }
                     });
                 });
--- web/api/logic/wxMenu.js
+++ web/api/logic/wxMenu.js
@@ -12,96 +12,6 @@ var config = require('../../config/config');
 var url = require('url');
 var wxMenu = {};
 
-wxMenu.defineWXMenu = function(appid, appsecret){
-    return function() {
-        var api = new WXAPI(appid, appsecret);
-        api.createMenu({
-            "button":
-                [
-                    {
-                        "name":"我的车",
-                        "sub_button":
-                            [
-                                {
-                                    "type":"click",
-                                    "name":"行车分析",
-                                    "key":"MYCAR.MYDRIVE"
-                                },
-                                {
-                                    "type":"click",
-                                    "name":"行车手册",
-                                    "key":"MY4S.MANUAL"
-                                },
-                                {
-                                    "type":"click",
-                                    "name":"远程检测",
-                                    "key":"MYCAR.MAINTAIN"
-                                },
-                                {
-                                    "type":"click",
-                                    "name":"行车报告",
-                                    "key":"MYCAR.COST"
-                                }
-                            ]
-                    },
-                    {
-                        "name":"我的4S",
-                        "sub_button":
-                            [
-                                {
-                                    "type":"click",
-                                    "name":"预约保养",
-                                     "key":"MY4S.BOOKING"
-    },
-                                {
-                                    "type":"click",
-                                    "name":"试乘试驾",
-                                    "key":"MY4S.PROBE"
-                                },
-                                {
-                                    "type":"click",
-                                    "name":"资讯活动",
-                                    "key":"MY4S.INFO"
-                                },
-                                {
-                                    "type":"click",
-                                    "name":"联系我们",
-                                    "key":"MY4S.CONTACT"
-                                }
-                            ]
-                    },
-                    {
-                        "name":"发现",
-                        "sub_button":
-                            [
-                                {
-                                    "type":"view",
-                                    "name":"我的活动",
-                                    "url": url.resolve(config.baseUrl, "msite/getCodeForActivity.html")
-                                },
-                                {
-                                    "type":"view",
-                                    "name":"我的预约",
-                                    "url": url.resolve(config.baseUrl, "msite/getCodeForBook.html")
-                                },
-                                {
-                                    "type":"view",
-                                    "name":"设置",
-                                    "url": url.resolve(config.baseUrl, "msite/getCodeForConf.html")
-                                }
-                            ]
-                    }
-                ]
-        }, function(err, result){
-            if (err) {
-                console.log('Error occurred when weixin menu was newly defined - ' + err + '\n');
-            }
-            else {
-                console.log('Weixin menu was newly defined!!\n');
-            }
-        });
-    };
-};
 var menuObject={
     "button":
         [
@@ -164,27 +74,23 @@ var menuObject={
                         {
                             "type":"view",
                             "name":"我的活动",
-                            "url": url.resolve(config.baseUrl, "msite/getCodeForActivity.html")
+                            "url": "msite/getCodeForActivity.html"
                         },
                         {
                             "type":"view",
                             "name":"我的预约",
-                            "url": url.resolve(config.baseUrl, "msite/getCodeForBook.html")
+                            "url": "msite/getCodeForBook.html"
                         },
                         {
                             "type":"view",
                             "name":"设置",
-                            "url": url.resolve(config.baseUrl, "msite/getCodeForConf.html")
+                            "url": "msite/getCodeForConf.html"
                         }
                     ]
             }]
 };
 wxMenu.defineTasks = function(tickTasks, callback) {
     //console.log("menu begins");
-    if(process.env.NODE_ENV === 'development'){
-        console.warn("Disable register wechat menu in development config");
-        return;
-    }
     menuBuilder(tickTasks, menuObject, callback);
 };
 wxMenu.textMsgRepliers = [];
