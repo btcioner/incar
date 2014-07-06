1056027d193d9c0953efc544805be574da0022c5
AceBear <vast_m_bear@yahoo.com>
2014-07-06 21:42:40 +0800

    调试代码

--- web/msite/mscripts/wxapp/WXOAuth.ts
+++ web/msite/mscripts/wxapp/WXOAuth.ts
@@ -7,23 +7,22 @@ module wxApp{
         }
 
         public findUserOpenId = (cb:(oid)=>void)=>{
-            if(!args.app_id) {
-                console.error("缺少app_id");
-                return;
-            }
-
             var args = this._$location.search();
             var wx_code = args.code;
             if(wx_code){
                 // 用CODE换取用户的open_id
                 var $injector = angular.injector(['wxApp']);
                 var $http = $injector.get('$http');
-                var postData = { app_id: args.app_id, code: wx_code };
+                var postData = { app_id: args.state, code: wx_code };
                 $http.post('/mservice/getOpenid' ,postData)
                     .success((data, status, headers, config)=>{ cb(data); })
                     .error((data, status, headers, config)=>{ console.error(status); });
             }
             else{
+                if(!args.app_id) {
+                    console.error("缺少app_id");
+                    return;
+                }
                 // 获取CODE
                 var redirect = "http://"+ this._$location.host() + this._$location.path();
                 var url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + args.app_id +
