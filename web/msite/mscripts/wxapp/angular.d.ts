48e97675a0234322f0437e932ff76abe253741bf
AceBear <vast_m_bear@yahoo.com>
2014-07-08 13:58:52 +0800

    require

--- web/msite/mscripts/wxapp/angular.d.ts
+++ web/msite/mscripts/wxapp/angular.d.ts
@@ -1,4 +1,4 @@
-// 辅助声明
+// angularjs declare
 declare var angular:{
     module: (moduleName:string, dependes:Array<any>)=>{
         controller: (ctrlName:string, init:Array<any>)=>any;
@@ -6,10 +6,14 @@ declare var angular:{
         filter:(name:string, factory:()=>any)=>any;
     };
     injector: (dependencies:Array<any>)=>any;
+    element: (arg:any)=>any;
+    bootstrap: (element:any, modules:Array<any>, config?:any)=>any;
     isUndefined: (value:any)=>boolean;
     isDefined: (value:any)=>boolean;
     forEach: (value:any, cb:(obj:any)=>any)=>any;
 };
 
-// jquery声明
+// jquery declare
 declare var $:any;
+// requirejs declare
+declare var requirejs:any;
