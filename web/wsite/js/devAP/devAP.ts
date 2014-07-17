e6e2c2a1593956f1c7870a819e943db643625a0e
AceBear <vast_m_bear@yahoo.com>
2014-07-17 10:06:21 +0800

    fix codeing

--- web/wsite/js/devAP/devAP.ts
+++ web/wsite/js/devAP/devAP.ts
@@ -16,14 +16,14 @@ module devAP {
         $locationProvider.html5Mode(true);
     }]);
 
-    // 页面控制�?
+    // 页面控制�?
     class DevAPCtrl {
         constructor(ctrlName:string) {
-            // 尝试读取以前的配�?
+            // 尝试读取以前的配�?
             if(localStorage && localStorage[this._lskURL]){
                 this.url = localStorage[this._lskURL];
             }
-            // 向angular注册控制�?
+            // 向angular注册控制�?
             _module.controller(ctrlName, ['$scope', '$sce', this.init]);
         }
 
@@ -67,10 +67,10 @@ module devAP {
 
         // 被测试的页面
         public pages = [
-            { title: "我的�?行车记录", url: "/msite/driveRecord.html" },
-            { title: "我的�?行车分析", url: "/msite/myDrive.html" },
-            { title: "我的�?行车手册", url: "/msite/page_xcsc.html" },
-            { title: "我的�?行车报告", url: "/msite/travelReport.html" },
+            { title: "我的车-行车记录", url: "/msite/driveRecord.html" },
+            { title: "我的车-行车分析", url: "/msite/myDrive.html" },
+            { title: "我的车-行车手册", url: "/msite/page_xcsc.html" },
+            { title: "我的车-行车报告", url: "/msite/travelReport.html" },
             { title: "我的4S-预约保养", url: "/msite/booking.html"  },
             { title: "我的4S-试乘试驾", url: "/msite/trialrun.html"  },
             { title: "我的4S-资讯活动", url: "/msite/my4sInfo.html" },
