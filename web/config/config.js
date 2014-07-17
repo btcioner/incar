9f23458aefae1f4fcbb574707ad5ea0e9e22eafe
AceBear <vast_m_bear@yahoo.com>
2014-07-17 22:14:39 +0800

    modify the dns name

--- web/config/config.js
+++ web/config/config.js
@@ -13,12 +13,12 @@ var rootPath = path.normalize(__dirname + '/..');
  * @returns {string}
  */
 function ConfigBaseURL(){
-    var url = 'http://www.incars.com.cn'; // 正式环境
+    var url = 'http://service.incardata.com.cn'; // 正式环境
 
-    if(process.env.NODE_ENV === 'development')
-        url = 'http://linuxsuse.chinacloudapp.cn'; // 当前开发环境AZURE-LINUX-SUSE, 以后切换到dev.incars.com.cn上
-    else if(process.env.NODE_ENV === 'staging')
-        url = 'http://114.215.172.92'; // 等备案OKAY后就切换为staging.incars.com.cn
+    if(process.env.NODE_ENV === 'staging')
+        url = 'http://staging.incardata.com.cn'; // 过渡环境
+    else if(process.env.NODE_ENV === 'development')
+        url = 'http://dev.incardata.com.cn';    // 开发环境
 
     return url;
 }
